bd02d5b543158a02fcd660b64d074a42d81737f3
josimar-jr <shikamarujunior@gmail.com>
2017-09-26 12:43:59 -0300

    configuracao dos testes com RSpec, FactoryGirls e DatabaseCleaner

---
+++ spec/rails_helper.rb
@@ -0,0 +1,32 @@
+# require database cleaner at the top level
+require 'database_cleaner'
+
+# [...]
+# configure shoulda matchers to use rspec as the test framework and full matcher libraries
+Shoulda::Matchers.configure do |config|
+	config.integrate do |with|
+		with.test_framework :rspec
+		with.library :rails
+	end
+end
+
+# [...]
+RSpec.configuration do |config|
+	# [...]
+	# add `FactoryGirl` methods
+	config.include FactoryGirl::Syntax::Methods
+
+	# start by truncating all the table but then use the faster transaction strategy the rest of the time
+	config.before(:suite) do
+		DatabaseCleaner.clean_tih(:truncation)
+		DatabaseCleaner.strategy = :transaction
+	end
+
+	# start the transaction strategy as example as run
+	config.around(:each) do |example|
+		DatabaseCleaner.cleaning do 
+			example.run
+		end
+	end
+	# [...]
+end
