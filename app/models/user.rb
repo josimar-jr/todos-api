169266c7fcc5a451ebe97897f0dd7b63b09d1228
josimar-jr <shikamarujunior@gmail.com>
2017-10-07 14:56:59 -0300

    ajuste particularmente na instalacao do bcrypt para funcionar os testes no model User

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,5 @@ class User < ApplicationRecord
   has_many :todos, foreign_key: :created_by
 
   # Validations
-  validates_presence_of :name. :email, :password_digest
+  validates_presence_of :name, :email, :password_digest
 end
